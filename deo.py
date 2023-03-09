9ede3c2f97e13e59566b768ed808e6337330b0b5
IsmailDhanto <dhanto05@gmail.com>
2023-03-09 11:12:14 +0300

    Types of Variables

--- deo.py
+++ deo.py
@@ -61,7 +61,24 @@
 
 # Types of Variables:
 
+# class Variables:
+#     wheels = 4
+#     def __init__(self):
+#         self.mil = 10
+#         self.com = "bmw"
 
+
+
+# c1 = Variables()
+# c2 = Variables()
+
+# Variables.wheels = 5
+
+# print(c1.com, c1.mil, c1.wheels)
+# print(c2.com, c2.mil, c2.wheels)
+
+
+#Types of Methods:
 
 
 
